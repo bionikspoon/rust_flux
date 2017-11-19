a8681dd9d3934d6e4b863a028f32ed3b929ac3f9
Manu Phatak <manu@thinkcerca.com>
2017-11-19 13:12:20 -0600

    use create-react-app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-async function init() {
-  const wasm = require('./main.rs')
-  const module = await wasm.initialize({ noExitRuntime: true })
-}
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-init()
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
+
+// async function init() {
+//   const wasm = require('./main.rs')
+//   const module = await wasm.initialize({ noExitRuntime: true })
+// }
+//
+// init()
